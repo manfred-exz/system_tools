e6199a7073ae08a9b97d1c8d45ef54113f80b7bb
manfred-exz <manfred-exz@users.noreply.github.com>
2023-03-27 11:38:11 +0800

    Update disable_wake_armed_devices.ps1

--- windows/disable_wake_armed_devices.ps1
+++ windows/disable_wake_armed_devices.ps1
@@ -1,4 +1,7 @@
 # Use this script, if you want to disable all devices(mouse/keyboard) to wake up your computer from suspend.
+# Note, you have to **disable all wake-armed-devices** to make it work. Otherwise, USB devices can still wake 
+# up your computer, even that USB device is not "wake-armed".
+
 # This may output the following error message, but it's OK:
 #   Unable to perform operation.  An unexpected error (0x490) has occurred:  Element not found.
 # OR
